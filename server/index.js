fc2641a1697e323e0ce4a46bc3a7766d40e842c8
Amela <amelacokaj@gmail.com>
2019-01-24 11:54:01 +0100

    project setup

---
+++ server/index.js
@@ -0,0 +1,34 @@
+'use strict';
+const path = require('path');
+const Hapi = require('hapi');
+const Inert = require('inert');
+
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+const init = async () => {
+
+    await server.register(require('inert'));
+    
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return h.file('./index.html');
+        }
+    });
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
